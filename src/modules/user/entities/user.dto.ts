37affd1e531d2729ef3491c6d0c2784d1fdb1d41
Enrico Battistella <enrico.battistella@gmail.com>
2024-01-24 16:08:48 +0100

    user

---
+++ src/modules/user/entities/user.dto.ts
@@ -0,0 +1,31 @@
+import { Type } from "class-transformer";
+import { IsDefined, IsNumber, IsObject, IsOptional, IsString, ValidateNested } from "class-validator";
+import { UserAmountSettings } from "./user.entity";
+
+class AmountSettingsDTO implements UserAmountSettings {
+  @IsOptional()
+  @IsNumber()
+  hourlyRate?: number;
+}
+
+class UserSettingsDTO {
+  @IsDefined()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => AmountSettingsDTO)
+  amount: AmountSettingsDTO;
+}
+
+export class CreateUserDTO {
+  @IsString()
+  name: string;
+
+  @IsDefined()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => UserSettingsDTO)
+  settings: UserSettingsDTO;
+  
+  @IsString()
+  company: string;
+}
---
+++ src/modules/user/entities/user.schema.ts
@@ -0,0 +1,32 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+import { UserAmountSettings } from "./user.entity";
+import { Schema as MongooseSchema } from 'mongoose'
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema({_id: false})
+class AmountSettings implements UserAmountSettings {
+  @Prop()
+  hourlyRate?: number;
+}
+
+const AmountSettingsSchema = SchemaFactory.createForClass(AmountSettings);
+
+@Schema()
+export class User {
+  id: string;
+
+  @Prop()
+  name: string;
+
+  @Prop({type: {amount: AmountSettingsSchema}, _id: false})
+  settings: {
+		amount: UserAmountSettings
+	};
+
+  @Prop({type: MongooseSchema.Types.ObjectId, ref: 'Company'})
+  company: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
