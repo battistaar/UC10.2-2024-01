1483e7a86b3238dc399bdab5ac486733703c1458
Enrico Battistella <enrico.battistella@gmail.com>
2024-01-25 14:18:13 +0100

    time-entry

--- src/modules/time-entry/entities/time-entry.dto.ts
+++ src/modules/time-entry/entities/time-entry.dto.ts
@@ -1,6 +1,21 @@
 import { Type } from "class-transformer";
-import { IsBoolean, IsDate, IsString } from "class-validator";
+import { IsBoolean, IsDate, IsDefined, IsNumber, IsObject, IsOptional, IsString, ValidateNested } from "class-validator";
 import { TimeEntry } from "./time-entry.schema";
+import { TimeEntryAmountSettings } from "./time-entry.entity";
+
+class AmountSettingsDTO implements TimeEntryAmountSettings {
+  @IsOptional()
+  @IsNumber()
+  hourlyRate?: number;
+}
+
+class TimeEntrySettingsDTO {
+  @IsDefined()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => AmountSettingsDTO)
+  amount: AmountSettingsDTO;
+}
 
 export class CreateTimeEntryDTO {
   @IsString()
@@ -16,6 +31,21 @@ export class CreateTimeEntryDTO {
 
   @IsBoolean()
   billable: boolean;
+
+  @IsString()
+  company: string;
+
+  @IsString()
+  project: string;
+
+  @IsString()
+  user: string;
+
+  @IsDefined()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => TimeEntrySettingsDTO)
+  settings: TimeEntrySettingsDTO;
 }
 
 export class TimeEntryResultDTO extends TimeEntry {
--- src/modules/time-entry/entities/time-entry.schema.ts
+++ src/modules/time-entry/entities/time-entry.schema.ts
@@ -1,7 +1,17 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
+import { Schema as MongooseSchema } from 'mongoose';
+import { TimeEntryAmountSettings } from './time-entry.entity';
 
 export type TimeEntryDocument = HydratedDocument<TimeEntry>;
+
+@Schema({_id: false})
+class AmountSettings implements TimeEntryAmountSettings {
+  @Prop()
+  hourlyRate?: number;
+}
+
+const AmountSettingsSchema = SchemaFactory.createForClass(AmountSettings);
 
 @Schema()
 export class TimeEntry {
@@ -18,6 +28,20 @@ export class TimeEntry {
 
   @Prop()
   billable: boolean;
+
+  @Prop({type: MongooseSchema.Types.ObjectId, ref: 'Company'})
+  company: string;
+
+  @Prop({type: MongooseSchema.Types.ObjectId, ref: 'Project'})
+  project: string;
+
+  @Prop({type: MongooseSchema.Types.ObjectId, ref: 'User'})
+  user: string;
+
+  @Prop({type: {amount: AmountSettingsSchema}, _id: false})
+  settings: {
+    amount: TimeEntryAmountSettings
+  };
 }
 
 export const TimeEntrySchema = SchemaFactory.createForClass(TimeEntry);
